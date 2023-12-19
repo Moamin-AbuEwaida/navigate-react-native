49e493433489a69b24b83d56a010cdd6c7449163
Moamin <moamin.salamah@gmail.com>
2023-12-19 11:20:47 +0100

    fixing apps routes

--- src/navigations/AuthNavigator.js
+++ src/navigations/AuthNavigator.js
@@ -1,16 +1,16 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
-import { Login, ForgetPassword, Register } from "../screens";
+import { Login, ForgotPassword, Register } from "../screens";
 import { ROUTES } from "../constants";
 
 const Stack = createStackNavigator();
 
 const AuthNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName={ROUTES.LOGIN}>
+      <Stack.Screen name={ROUTES.REGISTER} component={Register} />
       <Stack.Screen name={ROUTES.LOGIN} component={Login} />
-      {/* <Stack.Screen name={ROUTES.FORGOT_PASSWORD} component={ForgetPassword} />
-      <Stack.Screen name={ROUTES.REGISTER} component={Register} /> */}
+      <Stack.Screen name={ROUTES.FORGOT_PASSWORD} component={ForgotPassword} />
     </Stack.Navigator>
   );
 };
