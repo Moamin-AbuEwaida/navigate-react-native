88061e1fc55ab4d546c4c7a5943063e3ceb68524
Moamin <moamin.salamah@gmail.com>
2023-12-20 17:28:13 +0100

    bottom bar edit

---
+++ src/components/CustomTabBarButton.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CustomTabBarButton = () => {
+  return (
+    <View>
+      <Text>CustomTabBarButton</Text>
+    </View>
+  );
+};
+
+export default CustomTabBarButton;
--- src/navigations/BottomTabNavigator.js
+++ src/navigations/BottomTabNavigator.js
@@ -4,7 +4,8 @@ import { Home, Wallet, Notifications, Settings } from "../screens";
 import { ROUTES, COLORS } from "../constants";
 import Icon from "react-native-vector-icons/Ionicons";
 import SettingsNavigator from "./SettingsNavigator";
-
+import { StyleSheet } from "react-native";
+import CustomTabBarButton from "../components/CustomTabBarButton";
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
@@ -12,7 +13,9 @@ const BottomTabNavigator = () => {
     <Tab.Navigator
       screenOptions={({ route }) => ({
         headerShown: false,
-        tabBarShowLabel: false,
+        // tabBarShowLabel: false,
+        tabBarStyle: styles.tabBarStyle,
+        tabBarInactiveTintColor: COLORS.dark,
         tabBarActiveTintColor: COLORS.primary,
         tabBarIcon: ({ color, size, focused }) => {
           let iconName;
@@ -32,7 +35,13 @@ const BottomTabNavigator = () => {
         },
       })}
     >
-      <Tab.Screen name={ROUTES.HOME_TAB} component={Home} />
+      <Tab.Screen
+        name={ROUTES.HOME_TAB}
+        component={Home}
+        options={{
+          tabBarButton: (props) => <CustomTabBarButton {...props} />,
+        }}
+      />
       <Tab.Screen name={ROUTES.WALLET} component={Wallet} />
       <Tab.Screen name={ROUTES.NOTIFICATIONS} component={Notifications} />
       <Tab.Screen
@@ -47,3 +56,13 @@ const BottomTabNavigator = () => {
 };
 
 export default BottomTabNavigator;
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    position: "absolute",
+    backgroundColor: COLORS.transparent,
+    borderTopWidth: 0,
+    bottom: 15,
+    right: 10,
+    left: 10,
+  },
+});
